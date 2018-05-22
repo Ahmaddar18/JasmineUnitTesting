b700b642960940ee6d06e1580553340dc44b1e5a
ahmaddar18 <ahmaddar18@gmail.com>
2018-05-22 04:29:46 +0200

    my next commit

---
+++ spec/SciCodeSpec.js
@@ -0,0 +1,5 @@
+describe("SciCode", function(){
+	it("Should return string SciCode for input SciCode", function(){
+		expect(SciCodeInput("SciCode")).toEqual("SciCode");
+	})
+})
---
+++ src/SciCode.js
@@ -0,0 +1,4 @@
+function SciCodeInput()
+{
+	return "SciCode"
+}
